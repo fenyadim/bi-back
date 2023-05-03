c9765210b80764f71f55ae1bbaf26243f6da9728
fenyadim <dimos9813@gmail.com>
2023-05-03 08:35:30 +0800

    Refactoring

--- src/api/order/content-types/order/lifecycles.js
+++ src/api/order/content-types/order/lifecycles.js
@@ -1,44 +1,3 @@
-const { ViberClient } = require("messaging-api-viber");
-const ViberBot = require("viber-bot").Bot;
-const BotEvents = require("viber-bot").Events;
-const TextMessage = require("viber-bot").Message.Text;
-// const TextMessage = require("viber-bot").Message.Text;
-// const ViberBot = require("viber-bot").Bot;
-// const BotEvents = require("viber-bot").Events;
-
-// const client = new ViberClient({
-//   accessToken: authToken,
-//   sender: {
-//     name: "Sender",
-//   },
-// });
-const authToken = "50f097bd5d67e590-6d218ac0e2d544f-60b2336ed7f33e77";
-
-const bot = new ViberBot({
-  authToken: authToken,
-  name: "EchoBot",
-  avatar: "https://upload.wikimedia.org/wikipedia/commons/3/3d/Katze_weiss.png",
-});
-
-// Perfect! Now here's the key part:
-bot.on(BotEvents.MESSAGE_RECEIVED, (message, response) => {
-  // Echo's back the message to the client. Your bot logic should sit here.
-  response.send(message);
-});
-
-bot.onTextMessage(/^hi|hello$/i, (message, response) =>
-  response.send(
-    new TextMessage(`Hi there ${response.userProfile.name}. I am ${bot.name}`)
-  )
-);
-
-// Wasn't that easy? Let's create HTTPS server and set the webhook:
-const https = require("https");
-const { default: axios } = require("axios");
-const port = process.env.PORT || 8080;
-
-const webhookUrl = process.env.WEBHOOK_URL;
-
 module.exports = {
   async afterCreate(event) {
     // const { data } = event.params;
@@ -50,9 +9,5 @@ module.exports = {
       Адрес: `${data.city}, ${data.street}, ${data.house}, ${data.apartment}`,
       "Итоговая сумма": data.total,
     };
-    // Viber will push messages sent to this URL. Web server should be internet-facing.
-    https
-      .createServer(bot.middleware())
-      .listen(port, () => bot.setWebhook(webhookUrl));
   },
 };
