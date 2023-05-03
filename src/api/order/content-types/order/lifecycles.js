88d8a31aeee40cdadef3fdae6c4ddf4098d7a38e
fenyadim <dimos9813@gmail.com>
2023-05-03 14:20:57 +0800

    Refactoring

--- src/api/order/content-types/order/lifecycles.js
+++ src/api/order/content-types/order/lifecycles.js
@@ -1,13 +1,21 @@
+const axios = require("axios");
+
 module.exports = {
   async afterCreate(event) {
     // const { data } = event.params;
     const ctx = strapi.requestContext.get();
     const data = ctx.request.body.data;
     const res = {
-      Телефон: data.phone,
-      ФИО: data.name,
-      Адрес: `${data.city}, ${data.street}, ${data.house}, ${data.apartment}`,
-      "Итоговая сумма": data.total,
+      phone: data.phone,
+      fullName: data.name,
+      address: `${data.city}, ${data.street}, ${data.house}, ${data.apartment}`,
+      total: data.total,
     };
+    const message = `Телефон: ${res.phone}\nФИО: ${res.fullName}\nАдрес: ${res.address}\nИтоговая сумма: ${res.total}`;
+    console.log(JSON.stringify(res));
+    await axios.post(
+      `https://api.telegram.org/bot${env("BOT_ID")}/sendMessage`,
+      { chat_id: env("CHAT_ID"), text: message }
+    );
   },
 };
